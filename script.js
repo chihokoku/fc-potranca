a8b959185ec3a97ad734fc9ef10ab78d76fe0211
maccha <chihokoku8060@gmail.com>
2024-03-08 00:34:39 +0900

    add:comment

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ console.log('Hello');
 
 $('.burger').on('click', function () {
   if ($('.burger-nav').hasClass('burger-nav-open')) {
-    $('.burger-nav').removeClass('burger-nav-open');
-    // open-bg=バーガーを×にするクラス
-    $('.burger').removeClass('burger-open');
+    $('.burger-nav').removeClass('burger-nav-open');  //navを表示させるクラスを付与
+    $('.burger').removeClass('burger-open');  // バーガーを×にするクラス(burger-open)を付与
   } else {
     $('.burger-nav').addClass('burger-nav-open');
     $('.burger').addClass('burger-open');
