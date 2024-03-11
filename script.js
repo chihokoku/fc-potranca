3b43f68cf4a017bc03112acd947abc021b7489de
maccha <chihokoku8060@gmail.com>
2024-03-11 14:14:33 +0900

    complete:header&mainvisual

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-console.log('Hello');
+console.log("Hello");
 
-$('.burger').on('click', function () {
-  if ($('.burger-nav').hasClass('burger-nav-open')) {
-    $('.burger-nav').removeClass('burger-nav-open');  //navを表示させるクラスを付与
-    $('.burger').removeClass('burger-open');  // バーガーを×にするクラス(burger-open)を付与
+$(".burger").on("click", function () {
+  if ($(".burger-nav").hasClass("burger-nav-open")) {
+    $(".burger-nav").removeClass("burger-nav-open"); //navを表示させるクラスを付与
+    $(".burger").removeClass("burger-open"); // バーガーを×にするクラス(burger-open)を付与
+    $(".mask").removeClass("mask-open");
   } else {
-    $('.burger-nav').addClass('burger-nav-open');
-    $('.burger').addClass('burger-open');
+    $(".burger-nav").addClass("burger-nav-open");
+    $(".burger").addClass("burger-open");
+    $(".mask").addClass("mask-open");
   }
 });
