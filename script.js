d816e4a51accba45da0cc724065f73570d538c34
maccha <chihokoku8060@gmail.com>
2024-03-21 23:02:07 +0900

    add:nav-item click function

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ $(".burger").on("click", function () {
     $(".mask").addClass("mask-open");
   }
 });
+
+// バーガー内のナビメニューを押した際のアニメーション
+$(".nav-item a").on("click", function () {
+  $(".burger-nav").removeClass("burger-nav-open"); //navを表示させるクラスを付与
+  $(".burger").removeClass("burger-open"); // バーガーを×にするクラス(burger-open)を付与
+  $(".mask").removeClass("mask-open");
+});
